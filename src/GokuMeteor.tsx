4c6f1021295b43a980b266fb1777044894585bb8
yoshikouki <yoshikouki@gmail.com>
2021-11-27 22:27:02 +0900

    コンポーネントを用意

---
+++ src/GokuMeteor.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material'
+
+const GokuMeteor = () => {
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'darkblue',
+      }}
+    >
+    </Box>
+  )
+}
+
+export default GokuMeteor
