90f8233f44a9bf8d0af507a910c11883d428b395
yoshikouki <yoshikouki@gmail.com>
2021-11-27 22:56:45 +0900

    ポインタ自身と背景のコンポーネント

--- src/GokuMeteor.tsx
+++ src/GokuMeteor.tsx
@@ -1,16 +1,31 @@
 import { Box } from '@mui/material'
+import { styled } from '@mui/system'
 
 const GokuMeteor = () => {
   return (
-    <Box
-      sx={{
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: 'darkblue',
-      }}
-    >
-    </Box>
+    <MeteorBackground>
+      <MeteorTarget/>
+    </MeteorBackground>
   )
 }
+
+const MeteorBackground = styled(Box)({
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'darkblue',
+  cursor: 'none',
+})
+
+const MeteorTarget = styled('div')({
+  pointerEvents: 'none',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: 100,
+  width: 10,
+  height: 10,
+  backgroundColor: '#fff',
+  borderRadius: 5,
+})
 
 export default GokuMeteor
